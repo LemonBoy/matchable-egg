93a3936b9a3d4cfd1cdd7c3795beb6a6c34ead42
LemonBoy <thatlemon@gmail.com>
2016-04-01 15:43:24 +0200

    Use the future-proof interface exposed from lolevel

--- matchable.scm
+++ matchable.scm
@@ -1,26 +1,26 @@
 (module matchable *
   (import scheme chicken)
+  (use lolevel)
 
   ;; CHICKEN-specific glue
 
   ;; slot-ref type obj n
-  ;; Returns the 'n'th field of the record 'obj', we add 1 to the index since 
-  ;; the 0th field is reserved for the type.
+  ;; Returns the 'n'th field of the record 'obj'.
   (define-syntax slot-ref
     (syntax-rules ()
-      ((_ type obj n) (##sys#block-ref obj (add1 n)))))
+      ((_ type obj n) (record-instance-slot obj n))))
 
   ;; slot-set! type obj n val
   ;; Sets the value of the 'n'th field of the record 'obj' to 'val'.
   (define-syntax slot-set!
     (syntax-rules ()
-      ((_ type obj n val) (##sys#block-set! obj (add1 n) val))))
+      ((_ type obj n val) (record-instance-slot-set! obj n val))))
 
   ;; is-a? obj type
   ;; Returns #t if 'obj' is a record with name 'type', #f otherwise.
   (define-syntax is-a?
     (syntax-rules ()
-      ((_ obj type) (##sys#structure? obj (quote type)))))
+      ((_ obj type) (record-instance? obj (quote type)))))
 
   (include "match.scm")
 )
